6f14a4907780a8f943bdb5e130e360b581779167
Yanbo Huang <yanbo.huang@ing.com>
2018-07-10 16:27:18 +0200

    Initial commit

---
+++ buzz/generator.py
@@ -0,0 +1,23 @@
+import random
+
+BUZZ = ('continuous testing', 'continuous integration',
+        'continuous deployment', 'continuous improvement', 'devops')
+ADJECTIVES = ('complete', 'modern', 'self-service', 'integrated', 'end-to-end')
+ADVERBS = ('remarkably', 'enormously', 'substantially', 'significantly',
+           'seriously')
+VERBS = ('accelerates', 'improves', 'enhances', 'revamps', 'boosts')
+
+def sample(l, n = 1):
+    result = random.sample(l, n)
+    if n == 1:
+        return result[0]
+    return result
+
+def generate_buzz():
+    buzz_terms = sample(BUZZ, 2)
+    phrase = ' '.join([sample(ADJECTIVES), buzz_terms[0], sample(ADVERBS),
+                      sample(VERBS), buzz_terms[1]])
+    return phrase.title()
+
+if __name__ == "__main__":
+    print(generate_buzz())
